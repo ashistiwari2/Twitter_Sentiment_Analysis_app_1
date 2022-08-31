ce80bc7f228af550f43648e812cd757fcd98e5d2
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2022-08-31 16:50:07 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -27,10 +27,10 @@ accessTokenSecret=os.environ.get('ACCESSTOKENSECRET')
 
 try:
     # Create the authentication object
-    authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret))
+    authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
 
     # Set the access token and access token secret
-    authenticate.set_access_token(accessToken, accessTokenSecret))
+    authenticate.set_access_token(accessToken, accessTokenSecret)
     print(authenticate)
     # Creating the API object while passing in auth information
     api = tweepy.API(authenticate, wait_on_rate_limit=True)
